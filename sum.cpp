e9d66fddd2ac169997eb469ad80bacecb08041c0
SEOSLE <real2u2l8@gmail.com>
2025-03-05 12:06:09 +0900

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,8 +2,10 @@
 
 int sum(int n){
 	int res =0;
-	for (int i = 1; i <= n; i++){
+/*	for (int i = 1; i <= n; i++){ //시간복잡도 O(n)에서 O(1)로 바꿀 수 있음
 		res += i;
 	}
+*/	
+	res = n * (n + 1) / 2; //시간복잡도 O(1)
 	return res;
 }
